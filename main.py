f038983c1497e1ef4df6b0e831363ca73c8fca1f
jihune <jihun7891@naver.com>
2023-06-08 17:25:46 +0900

    all_craw_and_process_dart v1.0

---
+++ main.py
@@ -0,0 +1,159 @@
+import time
+import os
+import stock.filter_data as filter_data
+from stock.extract_data.extract import Extract
+from export_data import ExportToData
+import datetime
+
+this_year = datetime.datetime.now().year
+years = [this_year-2, this_year-1, this_year]
+
+def filterging_data(choice, data):
+    if choice == 1:
+        return filter_data.filtering_data_that_market_cap_under(0.3, data)
+    elif choice == 2:
+        return filter_data.filtering_data_that_market_cap_greater_than(0.8, data)
+    elif choice == 3:
+        return filter_data.filtering_data_that_market_index_kospi200(data)
+    elif choice == 4:
+        return filter_data.filtering_data_that_market_index_kosdaq150(data)
+    elif choice == 5:
+        return filter_data.filtering_data_that_market_index_krx300(data)
+    elif choice == 6:
+        return filter_data.filtering_data_that_market_index_test(data)
+
+def export_data(category, raw_data, extracted_data):
+    exporter = ExportToData()
+
+    directory = os.path.join(os.path.dirname(__file__), 'result_xlsx')
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # 현재 시간을 파일 이름에 추가합니다.
+    current_date = datetime.datetime.today().strftime('%Y%m%d')
+    current_time = datetime.datetime.now().strftime('%H%M')
+
+    file_name = f"{current_date}_{current_time}_{category}.xlsx"
+
+    file_path = os.path.join(directory, file_name)
+
+    exporter.export_to_excel_with_many_sheets(
+        file_path,
+        [
+            filter_data.filtering_low_per("ALL_DATA_저PER", raw_data.copy(), True),
+            filter_data.filtering_low_pbr_and_per("ALL_DATA_저PBR_저PER", 1.0, 10, raw_data.copy(), True),
+            filter_data.filtering_s_rim_disparity_all_data("S-RIM ALL_DATA", raw_data.copy()),
+            filter_data.filtering_low_per(f"{category}_저PER", extracted_data.copy()),
+            filter_data.filtering_low_pbr_and_per(f"{category}_저PBR_저PER", 1.0, 10, extracted_data.copy()),
+            filter_data.filtering_low_psr_and_per(f"{category}_저PSR_저PER", 10, extracted_data.copy()),
+            filter_data.filtering_peg(f"{category}_PEG", extracted_data.copy()),
+            filter_data.filtering_high_div("고배당률_리스트", raw_data.copy()),
+            filter_data.filtering_low_pfcr(f"{category}_저PFCR_시총잉여현금흐름", extracted_data.copy()),
+            filter_data.filtering_high_ncav_cap_and_gpa(f"{category}_고NCAV_GPA_저부채비율", extracted_data.copy()),
+            filter_data.filtering_s_rim_disparity_and_high_nav(f"{category}_S-RIM_괴리율_고NAV", extracted_data.copy()),
+            filter_data.filtering_profit_momentum(f"{category}_모멘텀_전분기대비_영업이익순이익_전략", extracted_data.copy()),
+            filter_data.filtering_value_factor(f"{category}_슈퍼가치_4가지_전략", extracted_data.copy()),
+            filter_data.filtering_value_and_profit_momentum(f"{category}_성장주모멘텀_전략", extracted_data.copy()),
+            filter_data.filtering_value_factor3(f"{category}_6가지_팩터순위합계", extracted_data.copy()),
+            filter_data.filtering_value_factor2(f"{category}_12가지_팩터순위합계", extracted_data.copy()),
+            filter_data.filtering_value_factor_upgrade(f"{category}_강환국_슈퍼가치전략_업글", extracted_data.copy()),
+
+            ("Extracted_RAW_Data", extracted_data),
+            ("RAW_Data", raw_data)
+        ]
+    )
+
+def craw_and_process(choice=None):
+
+    start = time.time()
+    sleep_time = 60
+    try_count  = 0
+
+    while True:
+        try:
+            if choice is None:
+                choice = int(
+                    input("소형주: 1 / 대형주: 2 / KOSPI200: 3 "
+                          "/ KOSDAQ150 : 4 / KRX300 : 5 / TEST: 6 / 번호를 입력하세요 => "))
+
+            extractor = Extract()
+
+            # calling kospi and kosdaq data using pykrx and OpenFinanceReader
+            kospi_kosdaq_data = extractor.get_data()
+
+            print("--------------")
+
+            # extract and calculating finance data recent 3 years data
+
+            if choice == 1:
+                print("소형주 스크래핑을 시작합니다 -------")
+                extracted_data = extractor.extract_finance_data(
+                    years,
+                    filterging_data(1, kospi_kosdaq_data)
+                )
+                export_data("소형주", kospi_kosdaq_data, extracted_data)
+            elif choice == 2:
+                print("대형주 스크래핑을 시작합니다 -------")
+                extracted_data = extractor.extract_finance_data(
+                    years,
+                    filterging_data(2, kospi_kosdaq_data)
+                )
+                export_data("대형주", kospi_kosdaq_data, extracted_data)
+
+            elif choice == 3:
+                print("KOSPI200 구성종목 스크래핑을 시작합니다 -------")
+                extracted_data = extractor.extract_finance_data(
+                    years,
+                    filterging_data(3, kospi_kosdaq_data)
+                )
+                export_data("KOSPI200", kospi_kosdaq_data, extracted_data)
+
+            elif choice == 4:
+                print("KOSDAQ150 구성종목 스크래핑을 시작합니다 -------")
+                extracted_data = extractor.extract_finance_data(
+                    years,
+                    filterging_data(4, kospi_kosdaq_data)
+                )
+                export_data("KOSDAQ150", kospi_kosdaq_data, extracted_data)
+
+            elif choice == 5:
+                print("KRX300 구성종목 스크래핑을 시작합니다 -------")
+                extracted_data = extractor.extract_finance_data(
+                    years,
+                    filterging_data(5, kospi_kosdaq_data)
+                )
+                export_data("KRX300", kospi_kosdaq_data, extracted_data)
+
+            elif choice == 6:
+                print("TEST로 삼성전자의 스크래핑을 시작합니다 -------")
+                extracted_data = extractor.extract_finance_data(
+                    years,
+                    filterging_data(6, kospi_kosdaq_data)
+                )
+                export_data("TEST", kospi_kosdaq_data, extracted_data)
+
+            else:
+                print("올바르지 않은 번호를 입력했습니다. 프로그램을 종료합니다.")
+                exit()
+
+            # If the execution reaches this point without errors, break the loop
+            break
+
+        except Exception as e:
+            print(f"현재시간 => {datetime.datetime.now()}")
+            print(f"크롤링 반복횟수: {try_count}회 (0회가 최초 반복의 시작점)")
+            print(f"에러 발생으로 {int(sleep_time / 60)}분 만큼 대기 시작: {e}")
+            time.sleep(sleep_time)
+            try_count += 1
+            sleep_time *= 2
+
+    end = time.time()
+    sec = (end - start)
+
+    result_list = str(datetime.timedelta(seconds=sec)).split(".")
+    print(f"현재시간 => {datetime.datetime.now()}")
+    print(f"크롤링 반복횟수: {try_count}회 (0회일 경우 반복 없이 크롤링 성공)")
+    print(f"Total extracting time: {result_list[0]} ---------------------")
+
+if __name__ == "__main__":
+    craw_and_process()
---
+++ stock/extract_data/basic_factor_data/korean_market_factor_data.py
@@ -0,0 +1,110 @@
+import datetime
+import pandas as pd
+import FinanceDataReader as fdr
+import stock.filter_data as filter_data
+from pykrx import stock
+
+class KoreanMarketFactorData:
+    def __init__(self):
+        self.stock = stock
+        self.fdr_data = fdr.StockListing("KRX")
+
+    def get_kospi_market_data(self):
+        """
+        종목코드, 종목명, 업종, BPS, PER, PBR, EPS, DIV, DPS가 담긴 데이터를 리턴.
+        :return: Pandas.DataFrame
+        """
+        return self.__get_fundamental_data("KOSPI")
+
+    def get_kosdaq_market_data(self):
+        """
+        종목코드, 종목명, 업종, BPS, PER, PBR, EPS, DIV, DPS가 담긴 데이터를 리턴.
+        :return: Pandas.DataFrame
+        """
+        return self.__get_fundamental_data("KOSDAQ")
+
+    def get_stock_ticker_and_name_list(self, market):
+
+        today = self.stock.get_nearest_business_day_in_a_week()
+
+        return filter_data.drop_column(self.__get_korean_stock_ticker_and_name(today, market))
+
+
+    def __get_fundamental_data(self, market):
+        """
+         종목코드, 종목명, 업종, 시가총액, 거래량, 거래대금, BPS, PER, PBR, EPS, DIV, DPS가 담긴 데이터를 리턴.
+         :return: Pandas.DataFrame
+         """
+        today = self.stock.get_nearest_business_day_in_a_week()
+
+        filtered_stock_ticker_and_name_list = filter_data.drop_column(self.__get_korean_stock_ticker_and_name(today, market))
+        stock_cap = self.__get_fundamental_data_market_cap(today)
+
+        temp_stock_list = pd.merge(filtered_stock_ticker_and_name_list, stock_cap, left_on="종목코드", right_on="종목코드")
+        stock_fundamental = self.__get_korean_stock_fundamental(today, market)
+
+
+        # 종목 코드로 조인
+        return pd.merge(temp_stock_list, stock_fundamental, left_on="종목코드", right_on="종목코드")
+
+    def __get_fundamental_data_market_cap(self, today):
+
+        stock_list = self.stock.get_market_cap(today)
+        stock_list = stock_list.reset_index()
+        stock_list.rename(columns={'티커': '종목코드'}, inplace=True)
+
+        return stock_list
+
+    def __get_korean_stock_ticker_and_name(self, date, market):
+        stock_list = pd.DataFrame({'종목코드': self.stock.get_market_ticker_list(date, market=market)})
+        stock_list['종목명'] = stock_list['종목코드'].map(lambda x: stock.get_market_ticker_name(x))
+        stock_list['업종'] = stock_list['종목명'].map(lambda x: self.fdr_data[self.fdr_data["Name"] == x]["Dept"].iloc[0])
+
+        return stock_list
+
+    def __get_korean_stock_fundamental(self, date, market):
+        stock_fud = pd.DataFrame(self.stock.get_market_fundamental(date, market=market))
+        stock_fud = stock_fud.reset_index()
+        stock_fud.rename(columns={'티커': '종목코드'}, inplace=True)
+
+        return stock_fud
+
+    def __get_korea_etf_ticker_and_name(self, date):
+        etf_list = pd.DataFrame({'종목코드': self.stock.get_etf_ticker_list(date)})
+        etf_list['종목명'] = etf_list['종목코드'].map(lambda x: stock.get_etf_ticker_name(x))
+
+        return etf_list
+
+    def __get_date(self):
+        """
+        :return weekdays
+        :return:
+        """
+        today = datetime.datetime.today().strftime("%Y%m%d")
+        year = str(datetime.datetime.today().strftime("%Y"))
+        month = str(datetime.datetime.today().strftime("%m"))
+        date = int(datetime.datetime.today().day)
+
+        date_formated = datetime.datetime.strptime(today, "%Y%m%d")  # datetime format 으로 변환
+
+        if date_formated.weekday() == 5:
+            if month == '12':
+                date -= 2  # 연말의 경우 2일을 뺀다.
+            else:
+                date -= 1  # 토요일일 경우 1일을 뺀다.
+        elif date_formated.weekday() == 6:
+            if month == '12':
+                date -= 3  # 연말의 경우 3일을 뺀다.
+            else:
+                date -= 2  # 일요일일 경우 2일을 뺀다.
+        elif date_formated.weekday() == 4 and month == '12':
+            date -= 1  # 연말인데 금요일이면 1일을 뺀다.
+
+            # 추석에 대한 처리
+        if month == '09' and year == '2020':
+            date -= 1
+        elif month == '09' and year == '2023':
+            date -= 3
+
+        return year + month + str(date).zfill(2)
+        # return '20220930'

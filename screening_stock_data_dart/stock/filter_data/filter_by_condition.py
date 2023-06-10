2d1c9503619664895fc2db0a5e8d6cafeed60c92
jihune <jihun7891@naver.com>
2023-06-11 01:33:03 +0900

    리츠 종목 Drop 코드 추가

--- screening_stock_data_dart/stock/filter_data/filter_by_condition.py
+++ screening_stock_data_dart/stock/filter_data/filter_by_condition.py
@@ -593,7 +593,7 @@ def drop_column(df: pd.DataFrame):
     )
     # 우선주 드랍
     df.drop(
-        df[df["종목명"].str.endswith(("우", "우B", "우C", "(전환)"))].index,
+        df[df["종목명"].str.endswith(("우", "우B", "우C", "(전환)", "리츠"))].index,
         inplace=True
     )
 
